54d963ddda3214247b6d58311685af844e335b0f
renanzucheratto <renan.loboz@gmail.com>
2026-02-26 20:17:07 -0300

    feat: limpando home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { Button } from "@mui/material";
-
 export default function Home() {
   return (
-    <div>
-      <Button color="primary" variant="contained">Teste</Button>
-    </div>
+    <>
+    </>
   );
 }
